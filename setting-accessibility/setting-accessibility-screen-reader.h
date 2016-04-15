861c0c98b7dab3d48ba3bf8d0e1863eb6a1e623b
sy1124.kim <sy1124.kim@samsung.com>
2016-04-15 16:12:29 +0900

    Remove mobile menu
    Change-Id: I2239e0b22a9b774ccec75a93359f7262c6ef0a7d
    Signed-off-by: Hyunjee Kim <hj0426.kim@samsung.com>

--- setting-accessibility/setting-accessibility-screen-reader.h
+++
@@ -1,9 +0,0 @@
-#ifndef SETTINGACCESSIBILITYSCREENREADER_H
-#define SETTINGACCESSIBILITYSCREENREADER_H
-
-#include "setting-accessibility.h"
-
-void setting_accessibility_screen_reader_page_create(SettingAccessibilityUG *data);
-
-#endif /* SETTINGACCESSIBILITYSCREENREADER_H */
-
